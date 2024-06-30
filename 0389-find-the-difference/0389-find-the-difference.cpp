96905fa41d7fc4891c03079408bd69ee674b842a
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2024-06-30 23:50:53 +0530

    Time: 4 ms (49.16%), Space: 7.7 MB (97.36%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char,int>mp;
+       //approach1
+        // unordered_map<char,int>mp;
 
-        for(char &ch :s){
-            mp[ch]++;
-        }
-        for(char &ch :t){
-            mp[ch]--;
-            if(mp[ch]<0){
-                return ch;
-            }
-        }
+        // for(char &ch :s){
+        //     mp[ch]++;
+        // }
+        // for(char &ch :t){
+        //     mp[ch]--;
+        //     if(mp[ch]<0){
+        //         return ch;
+        //     }
+        // }
         
-        return 'd';
-    }
+        // return 'd';
+
+    //     //approach 2 sum
+    //     int sum_s=0;
+    //     int sum_t=0;
+    //     for(char &ch : s){
+    //         sum_s+=ch;
+    //     }
+    //     for(char &ch : t){
+    //         sum_t+=ch;
+    //     }
+    //     return sum_t-sum_s;  
+
+    //approach 3 bits
+    char ans=0;
+    for(char &ch : s){
+         ans= ans^ch;
+        }
+        for(char &ch : t){
+           ans= ans^ch;
+        }
+        return ans;
+
+  }
 };
