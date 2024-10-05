52be628464ef8eb9e0ffdbeca4deecd6b9a1ba0f
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2024-10-05 16:59:40 +0530

    Time: 3 ms (55.84%), Space: 9.5 MB (47.7%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -10,15 +10,12 @@ public:
     }
 private:
    void helper(vector<int>& nums,set<vector<int>> &res, vector<int>& curr, int idx) {
-        res.insert(curr);  // Add current subset to the result
+        res.insert(curr);  
         
         for (int i = idx; i < nums.size(); ++i) {
-            // Skip duplicate elements
-       
-            
-            curr.push_back(nums[i]);  // Include nums[i] in the current subset
-            helper(nums, res, curr, i + 1);  // Recurse for further elements
-            curr.pop_back();  // Backtrack
+            curr.push_back(nums[i]);  
+            helper(nums, res, curr, i + 1);  
+            curr.pop_back();
         }
     }
 };
