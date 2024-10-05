07fa87b30e83690463c044d5494d76f8acc7eee6
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2024-10-05 16:59:14 +0530

    Time: 0 ms (100%), Space: 9.6 MB (39.34%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-     vector<vector<int>> res;
+    set<vector<int>> res;
         vector<int> curr;
         sort(nums.begin(),nums.end());
         helper(nums, res, curr, 0);
-        return res;
+         return vector<vector<int>>(res.begin(), res.end());
        
     }
 private:
-   void helper(vector<int>& nums, vector<vector<int>>& res, vector<int>& curr, int idx) {
-        res.push_back(curr);  // Add current subset to the result
+   void helper(vector<int>& nums,set<vector<int>> &res, vector<int>& curr, int idx) {
+        res.insert(curr);  // Add current subset to the result
         
         for (int i = idx; i < nums.size(); ++i) {
             // Skip duplicate elements
-            if (i > idx && nums[i] == nums[i - 1]) continue;
+       
             
             curr.push_back(nums[i]);  // Include nums[i] in the current subset
             helper(nums, res, curr, i + 1);  // Recurse for further elements
