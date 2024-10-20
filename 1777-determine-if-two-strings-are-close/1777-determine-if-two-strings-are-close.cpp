bf4b67f1e8b8a5d3da9e428a6c5e22e516a8bfe3
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2024-10-20 13:40:16 +0530

    Time: 57 ms (93.26%), Space: 23.9 MB (9.3%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
@@ -1,54 +1,32 @@
 class Solution {
 public:
-    bool closeStrings(string word1, string word2)
-    { 
-        map<char,int> m1;
-        map<char,int> m2;
-        if(word1.size()!=word2.size()) return false;
+    bool closeStrings(string word1, string word2) { 
+        if(word1.size() != word2.size()) 
+            return false;
 
-        for(int i=0;i<word1.size();i++)
-        {
-            m1[word1[i]]++;
-        }
-        
-        for(int i=0;i<word2.size();i++)
-        {
-            m2[word2[i]]++;
-        }
-        for(int i=0;i<word1.size();i++)
-        {
-            if(m2[word1[i]]==0)
-            {
+        map<char, int> m1, m2;
+
+        for(char c : word1) 
+            m1[c]++;
+        for(char c : word2) 
+            m2[c]++;
+
+        for(auto &p : m1) {
+            if(m2.find(p.first) == m2.end())
                 return false;
-            }
         }
-        for(int i=0;i<word2.size();i++)
-        {
-            if(m1[word2[i]]==0)
-            {
+        for(auto &p : m2) {
+            if(m1.find(p.first) == m1.end())
                 return false;
-            }
         }
 
-        vector<int> v1;
-        vector<int> v2;
-        
-        for(auto i : m1)
-        {
-            v1.push_back(i.second);
-        }
-      
-        
-        for(auto i : m2)
-        {
-            v2.push_back(i.second);
-        }
-        sort(v1.begin(),v1.end());
-        
-        sort(v2.begin(),v2.end());
+        unordered_multiset<int> set1, set2;
 
-        if(v1==v2) return true;
-        else return false;
+        for(auto &p : m1) 
+            set1.insert(p.second);
+        for(auto &p : m2) 
+            set2.insert(p.second);
 
+        return set1 == set2;
     }
 };
