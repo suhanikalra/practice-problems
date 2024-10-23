a12ae5e8861c7e24ac6672aa922c3352f474b5d7
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2024-10-23 13:02:02 +0530

    Time: 7 ms (42.45%), Space: 15 MB (28.04%) - LeetHub

---
+++ 0800-letter-case-permutation/0800-letter-case-permutation.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:void solve(string input,string op , vector<string> &output)
+    {   if(input.size()== 0){
+        output.push_back(op);
+        return;
+    }
+
+    if( isalpha(input[0])){
+        string op1 = op;
+        string op2 = op;
+        op1.push_back(tolower(input[0]));
+        op2.push_back(toupper(input[0]));
+        input.erase(input.begin()+0);
+         solve(input,op1,output);
+            solve(input,op2,output);
+    }
+
+    else{
+         string op1 = op;
+         op1.push_back(input[0]);
+         input.erase(input.begin()+0);
+         solve(input,op1,output);
+    }
+    }
+
+
+    vector<string> letterCasePermutation(string s) {
+        string input = s;
+        string op = "";
+        vector<string> output;
+        solve(input,op,output);
+        return output;}
+};
