527e073986be5d93e244f5f1d8409337e9d750ea
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2024-10-27 00:22:24 +0530

    Time: 23 ms (75.69%), Space: 16.3 MB (36.36%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -4,9 +4,9 @@ public:
         vector<vector<int>> ans;
 if(nums.size()<4 )return ans;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size() - 3; i++) {
+        for (int i = 0; i < nums.size() ; i++) {
             if(i>0 && nums[i]==nums[i-1])continue;
-            for (int j = i + 1; j < nums.size() - 2; j++) {
+            for (int j = i + 1; j < nums.size() ; j++) {
                 if(j>i+1 && nums[j]==nums[j-1])continue;
              long long temp = target;
                 int start = j + 1;
