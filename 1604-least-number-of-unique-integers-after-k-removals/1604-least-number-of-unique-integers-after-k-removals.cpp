e12212f6363f11162454ee8a62b7b5015c04d59f
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2024-10-27 17:06:20 +0530

    Time: 128 ms (32.56%), Space: 66.5 MB (56.6%) - LeetHub

--- 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.cpp
@@ -10,23 +10,19 @@ public:
             freq[a]++;
         }
 
-        auto cmp = [](const std::pair<int, int>& left, const std::pair<int, int>& right) {
-            return left.first > right.first;
-        };
-        
-        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, decltype(cmp)> minHeap(cmp);
+        std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
 
         for (const auto& entry : freq) {
-            minHeap.push({entry.second, entry.first});
+            minHeap.push(entry.second);
         }
 
         while (k > 0 && !minHeap.empty()) {
-            auto [frequency, number] = minHeap.top();
+            int frequency = minHeap.top();
             minHeap.pop();
             if (k >= frequency) {
                 k -= frequency;
             } else {
-                minHeap.push({frequency, number});
+                minHeap.push(frequency);
                 break;
             }
         }
