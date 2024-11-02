a3bcc893c1cd6d04f06bf0f2dd622bbcc51da490
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2024-11-03 01:30:44 +0530

    Time: 0 ms (100%), Space: 31.1 MB (30.37%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int minMoves(vector<int>& nums) {
         int count = 0;
-        // Find the minimum value in the array
+       
         int minNum = *min_element(nums.begin(), nums.end());
 
-        // Calculate the total moves required to equalize to the maximum value
+        
         for (const auto& num : nums) {
-            count += (num - minNum); // Count how many increments are needed
+            count += (num - minNum); 
         }
         
-        return count; // Return the total moves
+        return count; 
     }
 };
