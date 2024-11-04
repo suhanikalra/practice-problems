a5691d80da8871ada10a3458ded21be757814e46
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2024-11-05 00:37:06 +0530

    Time: 198 ms (33.33%), Space: 41 MB (35.82%) - LeetHub

--- 3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network/3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network.cpp
+++ 3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network/3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network.cpp
@@ -1,61 +1,70 @@
 class Solution {
 public:
-    int countReachableNodes(int root, int check, int signalSpeed, vector<vector<pair<int, int> > > &adj, int distance) {
-        int count = 0;
-
-        if (!(distance%signalSpeed)) count++;
-   
-        for (pair<int, int> p : adj[check]) {
-            if (p.first == root) continue;
-            else {
-                count += countReachableNodes(check, p.first, signalSpeed, adj, distance + p.second);
-            }
-        }
-        return count;
-    }
-    vector<int> countPairsOfConnectableServers(vector<vector<int>>& edges, int signalSpeed) {
+    int dfs(vector<vector<pair<int, int>>>& graph,vector<bool> &visited, int start, int currWeight,
+            int signalSpeed) {
         
-        unordered_set<int> s;
-        for (vector<int> v : edges) {
-            int n1 = v[0];
-            int n2 = v[1];
-            if (s.find(n1) == s.end()) s.insert(n1);
-            if (s.find(n2) == s.end()) s.insert(n2);
-        }
-        int n = s.size();
-        vector<vector<pair<int, int> > > adj(n);
-        for (vector<int> v : edges) {
-            int n1 = v[0];
-            int n2 = v[1];
-            int w = v[2];
-            adj[n1].push_back({n2, w});
-            adj[n2].push_back({n1, w});
-        }
-      
-        vector<int> res(n);
-        for (int i = 0; i < n; i++) {
-          
-            if (adj[i].size() < 2)  res[i] = 0;
-            else {
-               
-                vector<int> reachables;
-                for (pair<int, int> p : adj[i]) {
-              
-                    int reachable_nodes = countReachableNodes(i, p.first, signalSpeed, adj, p.second);
-                    if (reachable_nodes > 0 ) {
-                        reachables.push_back(reachable_nodes);
-                    }
-                }
-                int sum = 0;
-                int m = reachables.size();
-                for (int i = 0; i < m; i++) {
-                    for (int j = i+1; j < m; j++) {
-                        sum += reachables[i] * reachables[j];
-                    }
-                }
-                res[i] = sum;
+        int totalValue=0;
+        visited[start] = true;
+        if(currWeight%signalSpeed==0)totalValue++;
+        for (auto child : graph[start]) {
+
+            int node = child.first;
+            int wt = child.second;
+            if(visited[node]==false)
+            {
+                    totalValue +=
+                        dfs(graph, visited, child.first,
+                            currWeight + child.second, signalSpeed);
             }
+
         }
-        return res;
+        return totalValue;
+    }
+
+    vector<int> countPairsOfConnectableServers(vector<vector<int>>& edges,
+                                               int signalSpeed) {
+
+
+        int n = edges.size()+1; //edges+1=node
+
+        vector<vector<pair<int, int>>> graph(n);
+
+        for (auto x : edges) {
+            graph[x[0]].push_back({x[1], x[2]});
+            graph[x[1]].push_back({x[0], x[2]});
+        }
+   
+        vector<int> ans(n,0);
+        for (int i = 0; i < n; i++) {
+
+            vector<bool> visited(n,false);
+            vector<int> cnt; //will give tle if we predecleare size 
+            visited[i]=true;
+
+            if(graph[i].size()<2)continue;
+
+            for(auto child : graph[i])
+            {
+                int node = child.first;
+                int currWeight = child.second;
+
+                int currValue=dfs(graph,visited, node, currWeight, signalSpeed);
+                cnt.push_back(currValue);
+            }
+
+            int total=0;
+            for(int k=0;k<cnt.size();k++)
+            {
+                for( int j=k+1;j<cnt.size();j++)
+                {
+                    total=total+ cnt[k]*cnt[j];
+                }
+            }
+
+            ans[i]=total;
+
+        }
+
+        return ans;
     }
 };
