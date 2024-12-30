518c955b47be57858b4068a0fc4d5ecaebc5d206
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2024-12-30 12:26:55 +0530

    Time: 40 ms (88.61%), Space: 18.6 MB (38.74%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -4,7 +4,7 @@ public:
          if (matrix.empty() || matrix[0].empty()) return false;
         int top=0;
         int right= matrix[0].size()-1;
-        while(top<matrix.size() and right>0){
+        while(top<matrix.size() and right>=0){
             if(matrix[top][right]==target){return true;}
             else if(matrix[top][right]>target){
                 right--;
