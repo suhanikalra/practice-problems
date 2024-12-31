5775602f1d33984f8b2c85ca4337d5df65aec491
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2024-12-31 11:46:19 +0530

    Time: 16 ms (22.91%), Space: 16.5 MB (65.6%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-int solve(string &text1, string &text2,vector<vector<int>> &dp,int i,int j){
-        if(i==text1.size()|| j== text2.size()){
-            return 0;
-        }
-        if(dp[i][j] != -1 ) return dp[i][j];
+int solve(string &w1, string &w2,int i, int j,vector<vector<int>> &dp){
+    if(i==w1.size() || j== w2.size() )return 0;
+    if(dp[i][j]!=-1)return dp[i][j];
+    if(w1[i]==w2[j]) return dp[i][j]=solve(w1,w2,i+1,j+1,dp)+1;
+    
+    else  return dp[i][j]= max(solve(w1,w2,i+1,j,dp),solve(w1,w2,i,j+1,dp));
+}
+    int minDistance(string w1, string w2) {
+                vector<vector<int>> dp(w1.size(), vector<int>(w2.size(), -1));
 
-        if(text1[i]==text2[j]){
-          return dp[i][j]= solve(text1,text2,dp,i+1,j+1)+1;
-            
-        }
-        else{
-           return dp[i][j]= max(solve(text1,text2,dp,i+1,j),solve(text1,text2,dp,i,j+1));
-        }
-    }
-
-    int minDistance(string word1, string word2) {
-         int m= word1.size();
-        int n= word2.size();
-        vector<vector<int>> dp(m,vector<int>(n, -1));
-        return m+n-2*solve(word1,word2,dp,0,0);
+       return w1.size()+w2.size()-2*solve(w1,w2,0,0,dp);
     }
 };
