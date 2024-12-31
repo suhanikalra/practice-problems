18fb343a8ed426f808f766bf1d58720c60d557da
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2024-12-31 15:45:13 +0530

    Time: 0 ms (100%), Space: 18 MB (45.99%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        queue<pair<int,int>> q;
-        if(image[sr][sc] == color) return image;
-        int orignal= image[sr][sc];
-        image[sr][sc]=color;
-        q.push({sr,sc});
-        int dx[]={1,-1,0,0};
-        int dy[]= {0,0,1,-1};
-        while(!q.empty()){
-            auto ele= q.front();
-            int x= ele.first;
-            int y= ele.second;
-            q.pop();
-            
-            for(int i=0;i<4;i++){
-                auto newx= x+dx[i];
-                auto newy= y+dy[i];
-                if(newx>=0 and newy>=0 and newx<image.size() and newy<image[0].size() and image[newx][newy]==orignal){
-                    image[newx][newy]=color;
-                    q.push({newx,newy});
-                }
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int dx[], int dy[], int inicolor) {
+        image[sr][sc] = color;
+
+        for (int i = 0; i < 4; i++) {
+            if (sr + dx[i] >= 0 && sr + dx[i] < image.size() &&
+                sc + dy[i] >= 0 && sc + dy[i] < image[0].size() &&
+                image[sr + dx[i]][sc + dy[i]] == inicolor) {
+                dfs(image, sr + dx[i], sc + dy[i], color, dx, dy, inicolor);
             }
         }
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int dx[] = {1, 0, -1, 0};
+        int dy[] = {0, 1, 0, -1};
+        int inicolor = image[sr][sc];
+
+        if (inicolor != color) {
+            dfs(image, sr, sc, color, dx, dy, inicolor);
+        }
+
         return image;
     }
 };
