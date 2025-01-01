294a84755fef7ecfaa036f25f956aa1f556521fc
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-01 17:41:20 +0530

    Time: 2 ms (44.4%), Space: 9.2 MB (34.22%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int> ans;
-        vector<int> arr(26,0);
-       auto i=0;
-        for( char c: s){
-            arr[c-'a']=i++;
-        }
- int start = 0, end = 0;
-        
-        for (int i = 0; i < s.size(); i++) {
-            end = max(end, arr[s[i] - 'a']);
-            
-            if (i == end) {
-                ans.push_back(end - start + 1);
-                start = i + 1;
+        unordered_map<char,pair<int,int>>mp;
+        for(int i=0;i<s.size();i++){
+        if (mp.find(s[i]) != mp.end()) {
+                mp[s[i]].second=i;
             }
+            else mp[s[i]]={i,i};
+          
         }
-        return ans;
+        vector<pair<int,int>>p;
+        for(auto k: mp ){
+            p.push_back(k.second);
+        }
+        sort(p.begin(),p.end());
+
+        vector<int> result;
+    int prev_start = p[0].first;
+    int prev_end = p[0].second;
+
+    for (auto a : p) {
+        if (a.first > prev_end) { // Start a new partition
+            result.push_back(prev_end - prev_start + 1);
+            prev_start = a.first;
+            prev_end = a.second;
+        } else { // Merge intervals
+            prev_end = max(prev_end, a.second);
+        }
     }
+
+    // Add the last partition
+    result.push_back(prev_end - prev_start + 1);
+
+    return result;
+        }
+    
 };
