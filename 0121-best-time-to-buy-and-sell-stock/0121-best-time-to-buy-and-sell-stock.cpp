b6999c9fa1005133d2984541272b2e3354fb46e3
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-04 13:05:33 +0530

    Time: 0 ms (100%), Space: 97.3 MB (57.83%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,13 +3,12 @@ public:
     int maxProfit(vector<int>& prices) {
         if (prices.empty()) return 0;
 
-        int maxsofar=prices[prices.size()-1];
-        int profit=0;
-        for(int i = prices.size()-2; i >= 0; i--) { 
-
-        profit= max(profit,maxsofar-prices[i]);
-        maxsofar= max(maxsofar,prices[i]);
-        }
-        return profit;
+    int profit= 0;
+    int mini= INT_MAX;
+    for(auto k: prices){
+        mini= min(k,mini);
+        profit= max(k-mini, profit);
+    }
+    return profit;
     }
 };
