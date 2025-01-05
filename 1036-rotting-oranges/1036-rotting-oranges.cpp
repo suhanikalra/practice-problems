e81d00473fa266b3251b4b863175dcef10f73755
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-05 17:45:48 +0530

    Time: 0 ms (100%), Space: 16.8 MB (51.81%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<pair<int,int>,int>>q;int time=0;
-int fresh=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if (grid[i][j] == 1) {
-                    fresh++; }
-                if(grid[i][j]==2){q.push({{i,j},0});}
+        queue<pair<pair<int, int>, int>> q;
+        int fresh = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 2)
+                    q.push({{i, j}, 0});
+                if (grid[i][j] == 1)
+                    fresh++;
             }
         }
-        if (fresh == 0) return 0;
-    int dx[]= {-1,1,0,0};
-    int dy[]={0,0,1,-1};
+        int time = 0;
+        int dx[] = {1, -1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
+        while (!q.empty()) {
 
-
-        while(!q.empty()){
-            auto ele= q.front();
-            int x= ele.first.first;
-            int y= ele.first.second;
-              int distance= ele.second;
-             if(fresh==0)break;
-            q.pop();
-            for(int i=0;i<4;i++){
-                int newx= x+dx[i];
-                int newy= y+dy[i];
-                if(newx>=0 and newy>=0 and newx<grid.size() and newy<grid[0].size() and grid[newx][newy]==1){
-                    grid[newx][newy]=2;
-                     
-                q.push({{newx,newy},distance+1});
-                time= max(time,distance+1); 
-                    
-                    fresh--;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                auto ele = q.front();
+                int x = ele.first.first;
+                int y = ele.first.second;
+                int dist = ele.second;
+                q.pop();
+                for (int j = 0; j < 4; j++) {
+                    auto newx = x + dx[j];
+                    int newy = y + dy[j];
+                    if (newx >= 0 and newy >= 0 and newx < grid.size() and
+                        newy < grid[0].size() and grid[newx][newy] == 1) {
+                            grid[newx][newy] = 2;
+                        q.push({{newx, newy}, dist + 1});
+                        fresh--;
+                         time = max(time, dist + 1);
+                    }
                 }
             }
         }
-return (fresh == 0) ? time : -1;        
+        if (fresh == 0)
+            return time;
+        return -1;
     }
 };
