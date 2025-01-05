95edb1f39c47df7b5fb746c33614638525f8f569
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-06 00:27:37 +0530

    Time: 7 ms (43.94%), Space: 28.4 MB (59.33%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    vector<int> ngr(vector<int>& nums,vector<int> &ans, int n) {
-    
-   
-    stack<int> st;
-
-    for (int i = 2*n - 1; i >= 0; i--) {
-        while (!st.empty() && st.top() <= nums[i%n]) {
-            st.pop();
-        }
-        ans[i%n] = st.empty() ? -1 : st.top();
-        st.push(nums[i%n]);
-    }
-
-    return ans;
-}
-
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int n= nums.size();
-        vector<int>ans(n,-1);
-        return ngr(nums,ans,n);
+        stack<int> st;
+        vector<int> ans(nums.size(), -1);
+        int n = nums.size();
 
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while (!st.empty() and st.top() <= nums[i%n]) {
+                st.pop();
+            }
+            if (!st.empty() and st.top() > nums[i%n] and i<n) {
+                ans[i] = st.top();
+            }
+            st.push(nums[i % n]);
+        }
+
+        return ans;
     }
-
 };
