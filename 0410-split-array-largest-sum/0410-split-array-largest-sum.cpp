978e8691fb49b79e3db0045897f54a41b05d35df
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-06 19:14:05 +0530

    Time: 0 ms (100%), Space: 10.5 MB (40.28%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums, int k, int maxSumAllowed) {
-        int sum = 0, count = 1;  // Start with 1 subarray (since there's always at least 1)
-        
+bool solve(int mid,vector<int>& nums, int k){
+     int sum = 0;
+        int count = 1; 
         for (auto num : nums) {
-            sum += num;
-            if (sum > maxSumAllowed) {  // Need to split
-                sum = num;  // Start a new subarray
-                count++;    // Increment the subarray count
+            if (num > mid) return false; 
+            if (sum + num > mid) {
+                
+                count++;
+                sum = num;
+                if (count > k) return false;
+            } else {
+                sum += num;
             }
         }
-        
-        return count > k;  // Return true if we need more than k subarrays
+        return count <= k; 
     }
-
     int splitArray(vector<int>& nums, int k) {
-        int low = *max_element(nums.begin(), nums.end());  // At least the largest element
-        int high = accumulate(nums.begin(), nums.end(), 0);  // At most the sum of all elements
-        int ans = high;  // To store the result
+        int ans=0;
+        int start=0;int end=accumulate(nums.begin(), nums.end(), 0);
+       
+        while(start<=end){
+            int mid= (end-start)/2+start;
+            if(solve(mid,nums,k)==true){ans=mid;end=mid-1;}
+            else if(solve(mid,nums,k)!=true)start=mid+1;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (solve(nums, k, mid)) {  // If we need more than k subarrays, increase mid
-                low = mid + 1;
-            } else {  // Otherwise, it's a valid solution, try for a smaller max sum
-                ans = mid;
-                high = mid - 1;
-            }
         }
-
         return ans;
     }
 };
