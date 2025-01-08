63af8580a164337fde7bd0773e5fb445bb0e86cd
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-08 15:49:55 +0530

    Time: 24 ms (71.29%), Space: 88.1 MB (70.27%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-bool solve(int mid,vector<int>& candies, long long k){
+int solve(int mid,vector<int>& candies, long long p){
     long long count=0;
-    for(auto i: candies){
-        count+=i/mid;
+    for(auto k: candies){
+         count += k / mid;}
         
+return (count>=p);
     }
-    return count>=k;
-}
+
     int maximumCandies(vector<int>& candies, long long k) {
-        int start=1;int ans=0;
         long long sum=0;
-        for(auto k: candies)sum+=k;
-        int end= *max_element(candies.begin(),candies.end());
-        while(start<=end){
-            int mid= (end-start)/2+ start;
-            if(solve(mid,candies,k)==true){ans=mid;start=mid+1;}
-            else if(solve(mid,candies,k)==false){end=mid-1;}
+        for(auto p: candies)sum+=p;
+        if (sum < k) return 0;
+        int low= 1;
+        int high= *max_element(candies.begin(), candies.end()); 
+        int ans=candies[0];
+        while(low<=high){
+            int mid= (high - low)/2+ low;
+            if(solve(mid,candies,k)== true){ ans= mid; low=mid+1;}
+            else high= mid-1;
         }
         return ans;
-
     }
 };
