ad35d9fb976824f239d069fba8572a8c73d28e1c
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-09 20:05:09 +0530

    Time: 0 ms (100%), Space: 12.9 MB (33.93%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -15,7 +15,6 @@ public:
         if(p==NULL and q==NULL)return true;
         if(p==NULL || q==NULL)return false;
         if (p->val!=q->val) return false;
-        return isSameTree(p->left,q->left);
-        return isSameTree(p->right,q->right);
+        return isSameTree(p->left,q->left) and isSameTree(p->right,q->right);
     }
 };
