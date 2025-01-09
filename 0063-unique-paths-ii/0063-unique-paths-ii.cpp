5ea16c02996a8f6ca180ebf79ba12cdb4b13f01f
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-09 20:21:35 +0530

    Time: 3 ms (11.39%), Space: 12 MB (22.65%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-int solve(vector<vector<int>>& obstacleGrid,int i,int j,vector<vector<int>>&dp){
-       
-    if(i>=obstacleGrid.size() or j==obstacleGrid[0].size()  )return 0;
-     if(obstacleGrid[i][j]==1)return 0;
-        if(i==obstacleGrid.size()-1 and j==obstacleGrid[0].size()-1)return 1;
-        if(dp[i][j]!=-1)return dp[i][j];
-       return dp[i][j]=solve(obstacleGrid,i+1,j,dp)+ solve(obstacleGrid,i,1+j,dp);
+    int solve(int i, int j, vector<vector<int>>& dp, int m, int n,
+              vector<vector<int>>& obstacleGrid) {
 
+        if (i >= m or j >= n)
+            return 0;
+        if (obstacleGrid[i][j] == 1)
+            return 0;
+        if (i == m - 1 and j == n - 1)
+            return 1;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        return dp[i][j] = solve(i + 1, j, dp, m, n, obstacleGrid) +
+                          solve(i, j + 1, dp, m, n, obstacleGrid);
+    }
 
-}
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        vector<vector<int>>dp(obstacleGrid.size(),vector<int>(obstacleGrid[0].size(),-1));
-        return solve(obstacleGrid,0,0,dp);
+
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        if (obstacleGrid[m - 1][n - 1] == 1 || obstacleGrid[0][0] == 1)
+            return 0;
+
+        return solve(0, 0, dp, m, n, obstacleGrid);
     }
 };
