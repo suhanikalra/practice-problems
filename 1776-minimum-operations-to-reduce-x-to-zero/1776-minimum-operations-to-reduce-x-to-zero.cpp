4cbf9d8f764a6ea3c085b4218a425083753db232
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-11 12:52:34 +0530

    Time: 4 ms (63.5%), Space: 102.3 MB (56.08%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        int start=0;int end=0;long long sum=0;int maxi= INT_MIN;long long currentsum=0;
-        for(auto k: nums){
-            sum+=k;
-        }
+        long long sum=0;int maxi= INT_MIN;long long total=0;
+        for(auto k: nums)sum+=k;
         if(sum<x)return -1;
+
+        int start=0;int end=0;
         while(end<nums.size()){
-            currentsum+=nums[end];
-            while(currentsum>sum-x){
-                currentsum=currentsum-nums[start];
+            total+=nums[end];
+            while(total>sum-x){
+                total=total-nums[start];
                 start++;
+
             }
-            if(currentsum==sum-x)
-            maxi=max(maxi,end-start+1);
-            end++;
+            if(total== sum-x){
+                maxi=max(maxi,end-start+1);}end++;
+            
         }
-        if(maxi==INT_MIN)return -1;
-        return nums.size()-maxi;
+if(maxi==INT_MIN)return -1;
+        return nums.size()-maxi;        
     }
 };
