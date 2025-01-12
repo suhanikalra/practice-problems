9ee24d48a24ae8fd6f303fa8306b44b6469a1b83
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-12 15:31:48 +0530

    Time: 9 ms (19.81%), Space: 15.5 MB (51.18%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -10,62 +10,48 @@
 class Solution {
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        
-         queue<TreeNode*> q;
+        if(root==NULL or target==NULL)return {};
+        queue<TreeNode*>q;
+        unordered_map<TreeNode*,TreeNode*>parent;
+
         q.push(root);
-        TreeNode* temp = nullptr;
-        map<TreeNode*, TreeNode*> mp; 
-
-        
-        while (!q.empty()) {
-            TreeNode* node = q.front();
-            q.pop();
-
-            if (node->left) {
-                q.push(node->left);
-                mp[node->left] = node; 
+        while(!q.empty()){
+            int size= q.size();
+            for(int i=0;i<size;i++){
+                auto ele= q.front();
+                q.pop();
+                if(ele->left){parent[ele->left]=ele;q.push(ele->left);}
+                if(ele->right){parent[ele->right]=ele;q.push(ele->right);}
             }
             
-            if (node->right) {
-                q.push(node->right);
-                mp[node->right] = node;
-            }
         }
-
-        
-        //bfs on the treeee
-        set<TreeNode*>visited;
-        queue<TreeNode*> que;
-        que.push(target);
+        unordered_set<TreeNode*>visited;
+        q.push(target);
         visited.insert(target);
-int count=0;
-        while(!que.empty()){
-            if (count==k)break;
-           
-           
-             int size= que.size();
-            for(int i=0;i<size;i++){ auto ele= que.front();
-            que.pop();
-            if(ele and ele->left and visited.find(ele->left)==visited.end()){ que.push(ele->left);
-            visited.insert(ele->left);}
-             if(ele and ele->right and visited.find(ele->right)==visited.end()){ que.push(ele->right);
-             visited.insert(ele->right);}
-             if(visited.find(mp[ele])==visited.end()){que.push(mp[ele]);
-             visited.insert(mp[ele]);}
+        int count=0;
+        while(!q.empty()){
+             int size= q.size();
+             if(count==k)break;
+            for(int i=0;i<size;i++){
+                auto ele= q.front();
+                q.pop();
+                if(ele->left and visited.find(ele->left)==visited.end()){
+                    visited.insert(ele->left);q.push(ele->left);
+                }
+                if(ele->right and visited.find(ele->right)==visited.end()){
+                    visited.insert(ele->right);q.push(ele->right);
+                }
+                if (parent.find(ele) != parent.end() && visited.find(parent[ele]) == visited.end()) {
+                    visited.insert(parent[ele]);q.push(parent[ele]);}
+                    
+                }count++;
+
+        }vector<int>ans;
+        while(!q.empty()){
+            if(q.front())ans.push_back(q.front()->val);
+            q.pop();
         }
-        count++;
-
-        }
-
-
-vector<int> result;
-        while (!que.empty() and que.front() !=nullptr) {
-            result.push_back(que.front()->val);
-            que.pop();
-        }
-        
-     
-     return result;
-
+        return ans;
+   
     }
 };
