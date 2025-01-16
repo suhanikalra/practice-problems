6841f88ff2aeb90e788f4d6c4602e4376f21eac1
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-16 15:19:55 +0530

    Time: 0 ms (100%), Space: 27.3 MB (67.35%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -6,7 +6,8 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
@@ -14,30 +15,31 @@ public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         unordered_map<int, int> ind;
         for (int i = 0; i < inorder.size(); i++) {
-            ind[inorder[i]] = i; 
+            ind[inorder[i]] = i;
         }
-        int preorderInd = 0; 
-        return buildTreeHelper(preorder, inorder, ind, 0, inorder.size() - 1, preorderInd);
+        int preorderInd = 0;
+        return buildTreeHelper(preorder, inorder, ind, 0, inorder.size() - 1,
+                               preorderInd);
     }
 
 private:
-    TreeNode* buildTreeHelper(vector<int>& preorder, vector<int>& inorder, unordered_map<int, int>& ind, 
-                              int inorderStart, int inorderEnd, int& preorderInd) {
-       
+    TreeNode* buildTreeHelper(vector<int>& preorder, vector<int>& inorder,
+                              unordered_map<int, int>& ind, int inorderStart,
+                              int inorderEnd, int& preorderInd) {
+
         if (inorderStart > inorderEnd) {
             return nullptr;
         }
 
-       
         int rootVal = preorder[preorderInd++];
         TreeNode* root = new TreeNode(rootVal);
 
-        
         int inorderIndex = ind[rootVal];
 
-        
-        root->left = buildTreeHelper(preorder, inorder, ind, inorderStart, inorderIndex - 1, preorderInd);
-        root->right = buildTreeHelper(preorder, inorder, ind, inorderIndex + 1, inorderEnd, preorderInd);
+        root->left = buildTreeHelper(preorder, inorder, ind, inorderStart,
+                                     inorderIndex - 1, preorderInd);
+        root->right = buildTreeHelper(preorder, inorder, ind, inorderIndex + 1,
+                                      inorderEnd, preorderInd);
 
         return root;
     }
