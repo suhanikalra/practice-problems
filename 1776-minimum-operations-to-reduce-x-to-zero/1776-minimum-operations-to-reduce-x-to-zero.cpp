f8ee6b27b08f064de9c7dd0d106396a7d8888f49
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-18 10:37:06 +0530

    Time: 5 ms (53.16%), Space: 102.2 MB (83.92%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
-    int minOperations(vector<int>& nums, int x) {
-        long long sum=0;int maxi= INT_MIN;long long total=0;
-        for(auto k: nums)sum+=k;
-        if(sum<x)return -1;
-
-        int start=0;int end=0;
-        while(end<nums.size()){
-            total+=nums[end];
-            while(total>sum-x){
-                total=total-nums[start];
+    int solve(vector<int>& nums, long long& sum) {
+        int start = 0, end = 0;
+        long long curr = 0;
+        int maxi = 0;
+        
+        while (end < nums.size()) {
+            curr += nums[end]; 
+            while (curr > sum && start <= end) {
+                curr -= nums[start];
                 start++;
-
             }
-            if(total== sum-x){
-                maxi=max(maxi,end-start+1);}end++;
-            
+            if (curr == sum) {
+                maxi = max(maxi, end - start + 1);
+            }
+            end++;
         }
-if(maxi==INT_MIN)return -1;
-        return nums.size()-maxi;        
+        
+        return maxi;
+    }
+
+    int minOperations(vector<int>& nums, int x) {
+        long long sum = 0;
+        for (auto k : nums) {
+            sum += k;
+        }
+        if (sum < x) return -1;
+        sum -= x;
+        
+        int longestSubarray = solve(nums, sum);
+        if (longestSubarray == 0 && sum != 0) return -1; 
+        
+        return nums.size() - longestSubarray;
     }
 };
