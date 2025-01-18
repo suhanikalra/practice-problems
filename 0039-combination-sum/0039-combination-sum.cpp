2aa1d155e64abc879e3c959483b4fb752cad6afb
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-18 14:22:12 +0530

    Time: 76 ms (11.65%), Space: 65.5 MB (10.25%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
-void solve(vector<int>& cadinates, int target,vector<vector<int>>&ans,int index,vector<int> &temp){
-if(target==0) { ans.push_back(temp);}
-if(target<0  or index>=cadinates.size()) return;
-temp.push_back(cadinates[index]);
-solve(cadinates,target-cadinates[index],ans,index,temp);
-temp.pop_back();
-solve(cadinates,target,ans,index+1,temp);
-
-
-
-
-
+void solve(int i,vector<int>nums,int target,vector<vector<int>> &result,vector<int>&ans){
+if(i==nums.size())return;
+if(target<0)return;
+if(target==0){result.push_back(ans);return;}
+ans.push_back(nums[i]);
+solve(i,nums,target-nums[i],result,ans);
+ans.pop_back();
+solve(i+1,nums,target,result,ans);
 }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> result;vector<int>ans;
+        solve(0,nums,target,result,ans);
+        return result;
 
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;
-        vector<int> temp;
-        solve(candidates,target,ans,0,temp);
-      set<vector<int>> st;
-        for(auto p : ans){
-            st.insert(p);
-        }
-vector<vector<int>>anss;
-        for( auto k: st){
-            anss.push_back(k);
-        }
-        return anss;
+        
     }
 };
