7da7b1caa49de5bc67fc7df22481ec41fd72f5ad
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-18 10:04:54 +0530

    Time: 0 ms (100%), Space: 13 MB (90.55%) - LeetHub

--- 1380-number-of-closed-islands/1380-number-of-closed-islands.cpp
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& grid, int i, int j, vector<int> dx,
-               vector<int> dy) {
-                if(i<0 || j<0 || i>=grid.size() || j>= grid[0].size() || grid[i][j] !=0 )return;
-        if (grid[i][j] == 0)
-            grid[i][j] = 1;
-        for (int index = 0; index < 4; index++) {
-          solve(grid, i + dx[index], j + dy[index], dx, dy);
-        }
+void dfs(vector<vector<int>>& grid,int i,int j){
+    if(grid[i][j]==0){
+    grid[i][j]=2;}
+    int dx[]={1,-1,0,0};
+    int dy[]={0,0,-1,1};
+    for(int p=0;p<4;p++){
+        if(i+dx[p]>=0 and i+dx[p]<grid.size() and j+dy[p]>=0 and j+dy[p]<grid[0].size() and grid[i+dx[p]][j+dy[p]]==0)
+        dfs(grid,i+dx[p],j+dy[p]);
     }
-
+}
     int closedIsland(vector<vector<int>>& grid) {
-        vector<int> dx = {1, -1, 0, 0};
-        vector<int> dy = {0, 0, 1, -1};
-
         for (int i = 0; i < grid.size(); i++) {
-            if (grid[i][0] == 0)
-                solve(grid, i, 0, dx, dy);
-            if (grid[i][grid[0].size() - 1] == 0)
-                solve(grid, i, grid[0].size() - 1, dx, dy);
+            if (grid[i][0] == 0) dfs(grid, i, 0); 
+            if (grid[i][grid[0].size() - 1] == 0) dfs(grid, i, grid[0].size() - 1); 
         }
 
-        for (int i = 0; i < grid[0].size(); i++) {
-            if (grid[0][i] == 0)
-                solve(grid, 0, i, dx, dy);
-            if (grid[grid.size() - 1][i] == 0)
-                solve(grid, grid.size() - 1, i, dx, dy);
+        for (int j = 0; j < grid[0].size(); j++) {
+            if (grid[0][j] == 0) dfs(grid, 0, j); 
+            if (grid[grid.size() - 1][j] == 0) dfs(grid, grid.size() - 1, j); 
         }
-        int count = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == 0) {
-                    solve(grid, i, j, dx, dy);
-                    count++;
-                }
+int count=0;
+        for(auto i=1;i<grid.size()-1;i++){
+            for(int j=1;j<grid[0].size()-1;j++){
+                if(grid[i][j]==0){dfs(grid,i,j);count++;}
             }
         }
-        return count; 
+        return count;
+        
     }
 };
