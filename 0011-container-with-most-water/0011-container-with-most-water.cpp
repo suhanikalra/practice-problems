cc2f9bc50a939952dc3eaf0933cf89d3451171fd
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-19 15:55:32 +0530

    Time: 522 ms (5.53%), Space: 63.1 MB (23.69%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-        //https://leetcode.com/problems/count-primes/
-       
-        int start=0,  end= height.size()-1;
-int maxi=0,area=0;
+    int maxArea(vector<int>& nums) {
+        int start=0; int end= nums.size()-1;int maxi=0,area=0;
+
         while(start<=end){
-            area= min(height[start],height[end])*(end-start);
-            maxi= max(area,maxi);
-            if(height[start]<=height[end])start++;
+            area=min(nums[start],nums[end])*(end-start);
+            cout<<area;
+            maxi=max(area,maxi);
+            if(nums[start]<nums[end])start++;
             else end--;
         }
         return maxi;
+
     }
 };
