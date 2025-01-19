440af409aa2c1f2ac66b558551dbc4136cf91392
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-19 15:29:51 +0530

    Time: 96 ms (31.46%), Space: 81.7 MB (28.88%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
+int dist(vector<int>d){
+int distance= d[0]*d[0]+ d[1]*d[1];
+return distance;
+}
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<pair<int,pair<int,int>>>p;
-        for(auto k: points){
-            int d=k[0]*k[0]+k[1]*k[1];
-           p.push_back({d,{k[0],k[1]}});
-          
-           }
-            sort(p.begin(),p.end());
-            vector<vector<int>>ans;
-           for(int i=0;i<k;i++){
-            ans.push_back({p[i].second.first,p[i].second.second});
-        }
-        return ans;
+        vector<vector<int>>ans;
+        if(points.size()<k)return {};
+                    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> minheap;
+
+    for(auto k: points){
+        int d=dist(k);
+        minheap.push({d,{k[0],k[1]}});
+    }
+    while(k--){
+        auto ele= minheap.top();
+        ans.push_back({ele.second.first,ele.second.second});
+        minheap.pop();
+    }
+    return ans;
     }
 };
