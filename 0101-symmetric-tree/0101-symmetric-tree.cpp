c658c32fed071df13b0c0dafbb81f8b762f06066
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-19 18:23:09 +0530

    Time: 0 ms (100%), Space: 18.6 MB (23.35%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,18 +11,14 @@
  */
 class Solution {
 public:
-bool issimilar(TreeNode* p, TreeNode* q){
-    if(p==NULL and q== NULL)return true;
-    if(p==NULL or q== NULL)return false;
-    if(p->val!=q->val)return false;
-    return issimilar(p->left,q->right) and issimilar(p->right, q->left);
-
-}
+bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL and q==NULL)return true;
+        if(p==NULL or q==NULL)return false;
+        if(p->val != q->val)return false;
+       return isSameTree(p->left,q->right) and isSameTree(p->right,q->left);
+    }
     bool isSymmetric(TreeNode* root) {
         if(root==NULL)return true;
-        if(root->left==NULL and root->right==NULL)return true;
-        if(root->left==NULL || root->right==NULL)return false;
-        return issimilar(root->left,root->right);
-
+        return isSameTree(root->left,root->right);
     }
 };
