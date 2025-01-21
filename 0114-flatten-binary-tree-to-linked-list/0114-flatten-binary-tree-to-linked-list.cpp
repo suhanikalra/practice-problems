826e21f48cbc135d85952174cc24e1e436198091
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-21 12:25:52 +0530

    Time: 0 ms (100%), Space: 17.8 MB (14.27%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,19 +11,39 @@
  */
 class Solution {
 public:
-    void flatten(TreeNode* root) {
-        if(root==NULL)return ;
-        stack<TreeNode*>st;
-        st.push(root);
-        while(!st.empty()){
-            auto curr= st.top();
-            st.pop();
+void preorder( TreeNode* root,vector<int>&ans){
+	if(root==NULL)return;
+	ans.push_back(root->val);
+	preorder(root->left,ans);
+	preorder(root->right,ans);
+}
+void flatten(TreeNode* root) {
+    // add your logic here
+    if(root==NULL)return;
+	vector<int>ans;
+	preorder(root,ans);
+	if(root->left)root->left= NULL;
+	if(root->right)root->right=NULL;
+	
+	for(int i=1;i<ans.size();i++){
+		root->right = new TreeNode(ans[i]);
+		root->left=NULL;
+		root=root->right;
+	}
+}
+    // void flatten(TreeNode* root) {
+    //     if(root==NULL)return ;
+    //     stack<TreeNode*>st;
+    //     st.push(root);
+    //     while(!st.empty()){
+    //         auto curr= st.top();
+    //         st.pop();
 
-            if(curr->right)st.push(curr->right);
-            if(curr->left)st.push(curr->left);
-            if(!st.empty())curr->right= st.top();
-            if(curr->right)curr->left=NULL;
-        }
+    //         if(curr->right)st.push(curr->right);
+    //         if(curr->left)st.push(curr->left);
+    //         if(!st.empty())curr->right= st.top();
+    //         if(curr->right)curr->left=NULL;
+    //     }
         
-    }
+    
 };
