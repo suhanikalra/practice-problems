2f626cfde4fb505325d28f76f61807dbdb922c6c
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-23 12:56:29 +0530

    Time: 13 ms (1.06%), Space: 9.7 MB (5.85%) - LeetHub

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        unordered_map<int,int>mp;
+        for(auto k: word){
+            mp[k]++;
+            
+        }
+        vector<int>nums;
+        for(auto k: mp){
+            nums.push_back(k.second);
+        }
+        int count=0;
+        sort(nums.begin(),nums.end(),greater<int>());
+ for (int num : nums) {
+        cout << num << " ";
+    }       for(int i=1;i<nums.size();i++){
+        count+=nums[i-1]*(i/8+1);
+       }
+       return count+1;
+    }
+};
