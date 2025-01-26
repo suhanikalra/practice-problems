4378b4e7bee6ff93317e8b5203552bb1917b90db
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-01-26 15:19:59 +0530

    Time: 0 ms (100%), Space: 18.4 MB (34.75%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int dx[], int dy[], int inicolor) {
-        image[sr][sc] = color;
-
-        for (int i = 0; i < 4; i++) {
-            if (sr + dx[i] >= 0 && sr + dx[i] < image.size() &&
-                sc + dy[i] >= 0 && sc + dy[i] < image[0].size() &&
-                image[sr + dx[i]][sc + dy[i]] == inicolor) {
-                dfs(image, sr + dx[i], sc + dy[i], color, dx, dy, inicolor);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int originalColor = image[sr][sc];
+        
+        if (originalColor == color) return image;
+        
+        int rows = image.size();
+        int cols = image[0].size();
+        
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        image[sr][sc] = color;  
+        
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
+        
+        while(!q.empty()) {
+            auto ele = q.front();
+            q.pop();
+            int x = ele.first;
+            int y = ele.second;
+            
+            for(int i = 0; i < 4; i++) {
+                int newx = x + dx[i];
+                int newy = y + dy[i];
+                
+                if(newx >= 0 && newx < rows && 
+                   newy >= 0 && newy < cols && 
+                   image[newx][newy] == originalColor) {
+                    image[newx][newy] = color;
+                    q.push({newx, newy});
+                }
             }
         }
-    }
-
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int dx[] = {1, 0, -1, 0};
-        int dy[] = {0, 1, 0, -1};
-        int inicolor = image[sr][sc];
-
-        if (inicolor != color) {
-            dfs(image, sr, sc, color, dx, dy, inicolor);
-        }
-
+        
         return image;
     }
 };
