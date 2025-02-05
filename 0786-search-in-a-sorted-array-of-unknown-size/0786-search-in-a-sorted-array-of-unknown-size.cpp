8e8f9b8d0d5a7df1ebf59bd6077bdefe209fa907
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-02-05 13:24:44 +0530

    Time: 17 ms (98.27%), Space: 12.7 MB (27.27%) - LeetHub

---
+++ 0786-search-in-a-sorted-array-of-unknown-size/0786-search-in-a-sorted-array-of-unknown-size.cpp
@@ -0,0 +1,28 @@
+/**
+ * // This is the ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class ArrayReader {
+ *   public:
+ *     int get(int index);
+ * };
+ */
+
+class Solution {
+public:
+    int search(const ArrayReader& reader, int target) {
+        int start=0; int end= 1;
+        while(reader.get(end)<target){
+            end= end*2;
+
+        }
+        while(start<=end){
+            int mid= (end-start)/2+start;
+            if(reader.get(mid)<target)start= mid+1;
+             if(reader.get(mid)>target)end= mid-1;
+             if(reader.get(mid)==target)return mid;
+
+
+        }
+        return -1;
+    }
+};
