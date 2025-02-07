f075a076ba7c3337566127febe8d05f416780c72
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-02-08 02:27:02 +0530

    Time: 4 ms (24.19%), Space: 16.1 MB (24.55%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+       
+        vector<pair<int,int>>ans;
+        sort(intervals.begin(),intervals.end());
+
+for (int i = 1; i < intervals.size(); i++) 
+{            if (intervals[i][0] < intervals[i - 1][1]) {
+                return false; 
+            }
+
+        }
+        
+        return true;
+       
+
+    }
+};
