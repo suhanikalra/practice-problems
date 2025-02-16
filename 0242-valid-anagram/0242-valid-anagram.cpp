64db4ccfa7b0bd110b645c072790fa8be265cda5
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-02-16 20:51:48 +0530

    Time: 7 ms (22.87%), Space: 9.9 MB (26.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        if(s.size()!= t.size())return false;
+       unordered_map<char,int>mp;
+       for(auto k:s){
+        mp[k]++;
+       }
+       for(auto k:t){
+        if(mp[k]==0)return false;
+        mp[k]--;
+        if(mp[k]==0)mp.erase(k);
+       }
+       if(mp.size()==0)return true;
+
+       return false;
+
 
     }
 };
