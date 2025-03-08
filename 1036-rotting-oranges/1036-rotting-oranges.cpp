d824d0d165d7938bc0b7c43db8524a1d48a0f4f1
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-03-08 17:38:43 +0530

    Time: 2 ms (35.46%), Space: 16.9 MB (55.4%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-    queue<pair<pair<int, int>, int>> q;
-    int fresh=0;
+        queue<pair<pair<int,int>,int>>q;int fresh=0;int time=0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==1)fresh++;
                 if(grid[i][j]==2)q.push({{i,j},0});
+                if(grid[i][j]==1)fresh++;
             }
         }
-        int dx[]={1,-1,0,0};
-        int dy[]={0,0,1,-1};
-        int time=0;
         while(!q.empty()){
-            
-            
-                auto ele= q.front();
-                q.pop();
-                int x= ele.first.first;
-                int y= ele.first.second;
-                for(int i=0;i<4;i++){
-                    int newx=x+dx[i];
-                    int newy= y+dy[i];
-                    int dist=ele.second;
-                    if(newx>=0 and newy>=0 and newx<grid.size() and newy<grid[0].size() and grid[newx][newy]==1){grid[newx][newy]=2;
-                    time=max(time,dist+1);
-                    q.push({{newx,newy},dist+1});
-                    fresh--;
-
-                    
+            auto ele= q.front();
+            int x= ele.first.first;
+            int y= ele.first.second;
+            int dist= ele.second;
+            q.pop();
+            int dx[]= {1,-1,0,0};
+            int dy[]= {0,0,-1,1};
+            for(int i=0;i<4;i++){
+if(x + dx[i] >= 0 && y + dy[i] >= 0 && x + dx[i] < grid.size() && y + dy[i] < grid[0].size() && grid[x + dx[i]][y + dy[i]] == 1){
+                    q.push({{x+dx[i],y+dy[i]},dist+1});
+                    cout<<dist+1;grid[x+dx[i]][y+dy[i]]=2;
+                    time= max(dist+1,time); fresh--;
                 }
-
             }
-        }
-        if(fresh==0)return time;
+
+        }if(fresh==0)return time;
         return -1;
+        
     }
-
-
-
 };
