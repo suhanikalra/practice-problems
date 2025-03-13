1d44da3a73addf34bd7b3c770730e5607797adfc
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-03-14 00:18:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int prev=-1;
-        for(auto k: nums){
-            if(prev==k)return true;
-            prev=k;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++)
+            if(nums[i]==nums[j])return true;
         }
         return false;
+        
         
     }
 };
