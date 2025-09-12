98f706695e66837fc77e164d5574a6193bad3ed2
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2025-09-12 17:16:36 +0530

    Time: 1 ms (40.51%), Space: 16.8 MB (75.34%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<pair<int,int>,int>>q;int fresh=0;int time=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==2)q.push({{i,j},0});
-                if(grid[i][j]==1)fresh++;
-            }
-        }
-        while(!q.empty()){
-            auto ele= q.front();
-            int x= ele.first.first;
-            int y= ele.first.second;
-            int dist= ele.second;
-            q.pop();
-            int dx[]= {1,-1,0,0};
-            int dy[]= {0,0,-1,1};
-            for(int i=0;i<4;i++){
-if(x + dx[i] >= 0 && y + dy[i] >= 0 && x + dx[i] < grid.size() && y + dy[i] < grid[0].size() && grid[x + dx[i]][y + dy[i]] == 1){
-                    q.push({{x+dx[i],y+dy[i]},dist+1});
-                    cout<<dist+1;grid[x+dx[i]][y+dy[i]]=2;
-                    time= max(dist+1,time); fresh--;
+        int m = grid.size();
+        int n = grid[0].size();
+
+        queue<pair<pair<int,int>,int>> q; 
+        int freshCount = 0;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 2){
+                    q.push({{i,j},0});
+                }
+                else if(grid[i][j] == 1){
+                    freshCount++;
                 }
             }
+        }
 
-        }if(fresh==0)return time;
-        return -1;
-        
+        int time = 0;
+        int dx[] = {1,-1,0,0};
+        int dy[] = {0,0,1,-1};
+
+        while(!q.empty()){
+            auto ele = q.front(); q.pop();
+            int x = ele.first.first;
+            int y = ele.first.second;
+            int t = ele.second;
+            time = max(time, t);
+
+            for(int k=0; k<4; k++){
+                int newx = x + dx[k];
+                int newy = y + dy[k];
+                if(newx>=0 && newy>=0 && newx<m && newy<n && grid[newx][newy]==1){
+                    grid[newx][newy] = 2;
+                    freshCount--;
+                    q.push({{newx,newy}, t+1});
+                }
+            }
+        }
+
+        return freshCount == 0 ? time : -1;
     }
 };
