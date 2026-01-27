80052d4da750dd1ae7a1ea322389f376431dd38b
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-01-27 22:15:07 +0530

    Time: 0 ms (100%), Space: 71.6 MB (80.06%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,7 +4,8 @@ public:
         int maxsum=nums[0];
         int currsum=nums[0];
         for( int i=1; i<nums.size() ;i++){
-            currsum=max({nums[i],nums[i]+currsum});
+            if( currsum<0)currsum=0;
+            currsum=nums[i]+currsum;
             maxsum=max(currsum,maxsum);
         }
         return maxsum;
