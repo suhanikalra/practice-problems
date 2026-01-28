5e048315e78879c86fdcf70aa755f0c4a6ca5d74
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-01-28 18:09:26 +0530

    Time: 0 ms (100%), Space: 18.4 MB (95.33%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<vector<int>>v;
-        
+        vector<int>x;vector<int>y;
+
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
-                if(matrix[i][j]==0){
-                    for(int k=0;k<matrix[0].size();k++){
-            if(matrix[i][k] != 0)
-                    matrix[i][k]=INT_MIN+9;}
-                    for(int k=0;k<matrix.size();k++){
-             if(matrix[k][j] != 0)
-                    matrix[k][j]=INT_MIN+9;}
-                    
-                    }
-                }
+                if(matrix[i][j]==0){x.push_back(i);y.push_back(j);}
             }
-
-            for(int i=0;i<matrix.size();i++){
+        }
+        
+        for( auto i: x){
             for(int j=0;j<matrix[0].size();j++){
-                if(matrix[i][j]==INT_MIN+9){matrix[i][j]=0;}}}
+                matrix[i][j]=0;
+            
+        }}
 
-    
-///idk
+        for( auto i: y){
+            for(int j=0;j<matrix.size();j++){
+                matrix[j][i]=0;
+            
+        }
+        }
 
-    
-    
+        return;
+        
 
-    
+
     }
 };
