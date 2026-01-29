807e745b66e007516782458bd38ae1e0600dc1c8
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-01-30 00:03:49 +0530

    Time: 15 ms (20.57%), Space: 40.5 MB (11.16%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -6,26 +6,31 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-void solve(TreeNode* root,int targetSum,vector<int>&result,vector<vector<int>>&ans){
-     if(root==NULL)return ;
-        //preorder
-        result.push_back(root->val);
-        targetSum-=root->val;
-        if(root->left==NULL and root->right==NULL and targetSum==0){ans.push_back(result);}
-
-        if(root->left){solve(root->left,targetSum,result,ans);}
-        if(root->right){solve(root->right,targetSum,result,ans);}
-       result.pop_back();
+    vector<vector<int>> res;
+    void solve(TreeNode* root, int targetSum, vector<int> ans) {
+        if (root == NULL)
+            return;
         
-}
+        ans.push_back(root->val);
+        if (root->left == NULL && root->right == NULL &&
+            targetSum == root->val) {
+            res.push_back(ans);
+            return;
+        }
+        solve(root->left, targetSum - root->val, ans);
+        solve(root->right, targetSum - root->val, ans);
+    }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<int>result;vector<vector<int>>ans;
-       solve(root,targetSum,result,ans);
-       return ans;
+        if (root == NULL)
+            return {};
+        vector<int> ans;
+        solve(root, targetSum, ans);
+        return res;
     }
 };
