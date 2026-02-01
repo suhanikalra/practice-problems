318f6511473814e3820150369ccabc8c040bedf4
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-02-02 00:04:15 +0530

    Time: 19 ms (13.34%), Space: 19.2 MB (41.82%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -19,23 +19,21 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-      if(root==NULL)return NULL;
-      queue<Node*>q;
-      q.push(root);
-      while(!q.empty()){
-        int size= q.size();
-        for(int i=0;i<size;i++){
-            auto ele= q.front();
-            q.pop();
-            if(ele and i!=size-1)ele->next=q.front();
-            if(i==size-1)ele->next=NULL;
-           if(ele->left) q.push(ele->left);
-            if(ele->right) q.push(ele->right);
-        }
-      }
+        if(root==NULL)return NULL;
+        queue<Node*>q;
+        q.push(root);
 
- 
-      return root;
-        
+        while( !q.empty()){
+            int size= q.size();
+            for( int i=0;i<size;i++){
+                auto ele= q.front();q.pop();
+                if(i!=size-1)ele->next= q.front();
+                else ele->next= NULL;
+                if( ele->left){q.push(ele->left);}
+                if( ele->right){q.push(ele->right);}
+            }
+            
+        }
+        return root;
     }
 };
