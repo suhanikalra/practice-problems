465f37708bc36e54d474c4d53f5c2db406764a47
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-02-03 21:42:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-bool solve(int i, int target,vector<int>& nums,vector<vector<int>>&dp){
-    if(i>= nums.size())return false;
-    if(target<0)return false;
-    if(target==0)return true;
-    if(dp[i][target]!= -1)return dp[i][target];
-    return dp[i][target]=solve(i+1,target-nums[i],nums,dp) or solve(i+1,target,nums,dp);
+bool solve( int i, int sum,int curr,vector<int>& nums,vector<vector<int>>&dp){
+    if( i>=nums.size())return sum==curr;
+    if(curr>sum)return false;
+    if( curr==sum)return true;
+    return  solve( i+1,sum,curr+nums[i],nums,dp) or solve( i+1,sum,curr,nums,dp) ;
+    
 }
     bool canPartition(vector<int>& nums) {
         int sum=0;
-        for(auto k: nums)sum+=k;
-        if(sum%2!=0)return false;
- 
+        for (int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            
+        }
+        if( sum%2==1)return false;
         vector<vector<int>>dp(nums.size(),vector<int>(sum/2+1,-1));
-        
-      return solve(0,sum/2,nums,dp);
+
+        return solve( 0,sum,0,nums,dp);
     }
 };
