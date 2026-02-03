9e34481febb7c4f21eda81b03d5a82f63b99ab43
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-02-03 11:42:43 +0530

    Time: 15 ms (67.97%), Space: 47.2 MB (58.38%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -9,7 +9,7 @@ const int MOD = 1e9 + 7;
         stack<int>rightst;
 
         for(int i=0;i<n;i++){
-            while(!leftst.empty() and arr[leftst.top()]>arr[i])leftst.pop();
+            while(!leftst.empty() and arr[leftst.top()]>=arr[i])leftst.pop();
             
            if(!leftst.empty()) nsl[i]=leftst.top();
            leftst.push( i);
@@ -17,7 +17,7 @@ const int MOD = 1e9 + 7;
         }
 
         for( int i=n-1;i>=0;i--){
-            while(!rightst.empty() and arr[rightst.top()]>=arr[i])rightst.pop();
+            while(!rightst.empty() and arr[rightst.top()]>arr[i])rightst.pop();
             if(!rightst.empty()) nsr[i]=rightst.top();
            rightst.push( i);
 
