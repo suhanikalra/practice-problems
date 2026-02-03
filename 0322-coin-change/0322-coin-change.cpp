209d572d3a01cd8f161bc4b6ba68b83c5b6877b2
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-02-03 22:07:53 +0530

    Time: 59 ms (15.01%), Space: 49.8 MB (20.08%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,16 +1,17 @@
 class Solution {
-public: 
-int solve(int i, vector<int>& coins, int amount, vector<vector<int>>&dp){
-    if(amount==0){return 0;}
-    if(i==coins.size() || amount<0 )return 100000 ;
-    if(dp[i][amount]!=-1)return dp[i][amount];
+public:
+int solve(int i,vector<int>& coins, int amount, vector<vector<int>>&dp){
+    if( amount==0) return 0;
+    if( i>=coins.size())return 100000;
+    if(amount<0)return 100000;
     
+    if(dp[i][amount]!=-1)return dp[i][amount];
 
-    return dp[i][amount]=min(solve(i,coins,amount-coins[i],dp)+1, solve(i+1,coins,amount,dp));
+    return dp[i][amount]= min(  solve(i, coins,amount-coins[i],dp)+1, solve(i+1, coins,amount,dp));
 }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
-        if(solve(0,coins,amount,dp)!=100000)return solve(0,coins,amount,dp);
-        else return -1;
+         vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
+        int ans = solve(0, coins, amount, dp);
+        return (ans >= 100000) ? -1 : ans;
     }
 };
