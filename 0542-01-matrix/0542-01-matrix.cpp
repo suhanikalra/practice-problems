9fe98c0adbfb663cf397389602510a538daa378b
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-02-05 12:59:47 +0530

    Time: 15 ms (72.31%), Space: 36.1 MB (56.8%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        vector<vector<int>>ans(mat.size(),vector<int>(mat[0].size(),-1));
-       
-        queue<pair<pair<int, int>, int>> q;
-         for(int i=0;i<mat.size();i++){
-            for(int j=0;j<mat[0].size();j++){
-                if(mat[i][j]==0){ans[i][j]=0;
-                q.push({{i,j},0});}
+        int n= mat.size();int m= mat[0].size();
+        queue<tuple<int,int,int>>q;//x//y,dist
+        vector<vector<int>> dis(n, vector<int>(m, INT_MAX));
+        for(int i=0;i<mat.size();i++){
+            for( int j=0;j<mat[0].size();j++){
+               if( mat[i][j]==0) {q.push({i,j,0});
+               dis[i][j]=0;}
             }
         }
-int dx[]={0,0,1,-1};
-int dy[]={1,-1,0,0};
-        while(!q.empty()){
-            
-            auto ele= q.front();
-            int x= ele.first.first;
-            int y= ele.first.second;
-            int distance= ele.second;
+int dx[]= {1,-1,0,0};
+int dy[]= {0,0,-1,1};
+        while( !q.empty()){
+            auto size= q.size();
+            auto [x, y, dist] = q.front();
             q.pop();
-
-            for(int i=0;i<4;i++){
-                int newx= x+dx[i];
-                int newy= y+dy[i];
-                if(newx>=0 and newx<mat.size() and newy>=0 and newy<mat[0].size() and mat[newx][newy]==1 and ans[newx][newy]==-1){
-                ans[newx][newy]= distance+1;
-                
-                q.push({{newx,newy},distance+1});}
-
+            for( int i=0;i<4;i++){
+                int newx=x+dx[i];int newy=y+dy[i];
+                if( newx>=0 and newx<n and newy>=0 and newy<m and dis[newx][newy]==INT_MAX and mat[newx][newy]==1){
+                    dis[newx][newy]=dist+1;
+                    q.push( { newx,newy,dist+1});
+                }
             }
-
+           
             
+
         }
-        return ans;
-       
+        return dis;
     }
 };
