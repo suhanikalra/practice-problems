a0c73fdbfbace1b88ad14dfc208b9adf1dc140a9
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-02-05 14:19:45 +0530

    Time: 5 ms (9%), Space: 17.1 MB (31.81%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        set<pair<int,int>> vis;
+        queue<pair<pair<int,int>, int>> q;
+        int fresh = 0;
+
+        int n = grid.size(), m = grid[0].size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    q.push({{i, j}, 0});
+                    vis.insert({i, j});
+                }
+                if (grid[i][j] == 1) fresh++;
+            }
+        }
+
+        int dx[] = {0, 0, 1, -1};
+        int dy[] = {1, -1, 0, 0};
+        int ans = 0;
+
+        while (!q.empty()) {
+            auto ele = q.front(); q.pop();
+            int x = ele.first.first;
+            int y = ele.first.second;
+            int dist = ele.second;
+            ans = max(ans, dist);
+
+            for (int i = 0; i < 4; i++) {
+                int newx = x + dx[i];
+                int newy = y + dy[i];
+
+                if (newx >= 0 && newx < n &&
+                    newy >= 0 && newy < m &&
+                    grid[newx][newy] == 1 &&
+                    vis.find({newx, newy}) == vis.end()) {
+
+                    grid[newx][newy] = 2;
+                    vis.insert({newx, newy});
+                    fresh--;
+                    q.push({{newx, newy}, dist + 1});
+                }
+            }
+        }
+
+        return fresh == 0 ? ans : -1;
+    }
+};
