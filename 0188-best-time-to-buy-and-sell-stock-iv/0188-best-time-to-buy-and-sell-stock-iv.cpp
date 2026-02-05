1fe4742b700c5af6049cc7fc4ff1ee99816ffb58
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-02-05 14:41:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,34 +1,16 @@
-
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
+int solve( int i,int buy, int count,int k, vector<int>& prices){
+    if( i==prices.size() or count==k)return 0;
+    if( buy){
+        return max((-prices[i]+ solve( i+1,0,count,k,prices )),solve( i+1,1,count,k,prices ));}
 
-    int solve(int i, int buy, int k,int count, vector<int>& prices) {
-        if (i == prices.size() || k == count)
-            return 0;
+    else return max({ (prices[i]+ solve( i+1,1,count++,k,prices )),solve( i+1,0,count,k,prices )});
 
-        if (dp[i][buy][count] != -1)
-            return dp[i][buy][count];
-
-        if (buy) {
-            return dp[i][buy][count] = max(
-                -prices[i] + solve(i + 1, 0, k,count, prices), // buy
-                 solve(i + 1, 1, k,count, prices)               // skip
-            );
-        } else {
-            return dp[i][buy][count] = max(
-                prices[i] + solve(i + 1, 1,k, count + 1, prices), 
-                solve(i + 1, 0, k,count, prices)                  
-            );
-        }
     }
 
     int maxProfit(int k, vector<int>& prices) {
-        
-    int n = prices.size();
-        dp = vector<vector<vector<int>>>(n,
-              vector<vector<int>>(2, vector<int>(k, -1)));
+        return solve( 0,1,0,k,prices );//index,buy/sell,count
 
-        return solve(0, 1, k,0, prices);
-}
+    }
 };
