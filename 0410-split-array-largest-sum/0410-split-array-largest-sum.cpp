8646aea25133ba11dfd3a3cda65894dbc788a702
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-02-06 09:57:29 +0530

    Time: 2 ms (9.24%), Space: 10.3 MB (90.33%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-bool solve(int mid,vector<int>& nums, int k){
-     int sum = 0;
-        int count = 1; 
-        for (auto num : nums) {
-            if (num > mid) return false; 
-            if (sum + num > mid) {
-                
-                count++;
-                sum = num;
-                if (count > k) return false;
-            } else {
-                sum += num;
-            }
+int solve( int mid,vector<int>& nums, int k ){
+    int count=1;int sum=0;
+    for( int i=0;i<nums.size();i++){
+        if(nums[i]>mid)return false;
+        sum+=nums[i];
+        if( sum>mid){sum=nums[i];count++;
         }
-        return count <= k; 
-    }
-    int splitArray(vector<int>& nums, int k) {
-        int ans=0;
-        int start=0;int end=accumulate(nums.begin(), nums.end(), 0);
-       
-        while(start<=end){
-            int mid= (end-start)/2+start;
-            if(solve(mid,nums,k)==true){ans=mid;end=mid-1;}
-            else if(solve(mid,nums,k)!=true)start=mid+1;
 
+        
+    }
+    return count<=k;
+}
+    int splitArray(vector<int>& nums, int k) {
+     int low=0; int high = accumulate(nums.begin(), nums.end(), 0);int ans=-1;
+        int mid= ( high-low)/2+low;
+        while( low<=high){
+            mid= ( high-low)/2+low;
+            if (solve(mid, nums, k)){ans=mid;high= mid-1;}
+            else{
+                low=mid+1;
+            }
         }
         return ans;
     }
