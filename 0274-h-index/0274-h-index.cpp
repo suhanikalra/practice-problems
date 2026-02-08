933828063c872282d8d97c7a8e98c5ae197a9001
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-02-08 21:41:45 +0530

    Time: 0 ms (100%), Space: 12.1 MB (98.01%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        //can u explain me this ques
-        sort(citations.begin(),citations.end(),greater<int>());
-        int i;int ans=0;
-        for(i=0;i<citations.size();i++){
-            int index= i+1;
-            if(citations[i]>=index )ans= index;
-            else break;
+        sort(citations.begin(),citations.end(),greater<int>() );
+        int low= 0;int high= citations.size()-1;int ans=0;
+        int mid= (high-low)/2+ low;
+
+        while( low<=high){
+            mid= (high-low)/2+ low;
+            if(citations[mid]>=mid+1 ){ans=mid+1;low=mid+1;}
+            else{
+                high= mid-1;
+            }
         }
-        
         return ans;
+
     }
 };
+
+//[3,0,6,1,5]
