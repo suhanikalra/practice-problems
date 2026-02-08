d7243ef3bdb9717f7beb65ab61097d9d86375b88
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-02-08 17:18:26 +0530

    Time: 331 ms (47.25%), Space: 11.7 MB (31.55%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    int n, m;
+bool ans= false;
+void dfs( int index,int i,int j,vector<vector<char>>& board, string word){
+    if( index>=word.size()-1){ans= true;return;}
+    if( board[i][j]==word[index]){
+        auto temp=  board[i][j];
+        board[i][j]='$';
 
-    bool check(int i, int j, int k, vector<vector<char>>& board, string& word) {
-        if (k == word.size()) return true;
+        int dx[]={0,0,1,-1};
+        int dy[]= {1,-1,0,0};
 
-        if (i < 0 || j < 0 || i >= n || j >= m ||
-            board[i][j] != word[k])
-            return false;
+        for( int ind=0;ind<4;ind++){
+            auto newx=i+dx[ind];
+            auto newy= j+ dy[ind];
+            if( newx>=0 and newx<board.size() and newy>=0 and newy<board[0].size() and board[newx][newy]==word[index+1])dfs(index+1,newx,newy, board,word);
+        }
+         board[i][j]=temp;
 
-        char temp = board[i][j];
-        board[i][j] = '#'; 
-        bool found =
-            check(i+1, j, k+1, board, word) ||
-            check(i-1, j, k+1, board, word) ||
-            check(i, j+1, k+1, board, word) ||
-            check(i, j-1, k+1, board, word);
-
-        board[i][j] = temp; 
-        return found;
     }
-
+}
     bool exist(vector<vector<char>>& board, string word) {
-        n = board.size();
-        m = board[0].size();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == word[0] &&
-                    check(i, j, 0, board, word))
-                    return true;
+        
+        for( int i=0;i<board.size();i++){
+            for( int j=0;j<board[0].size();j++){
+                if( board[i][j]==word[0] ){
+                    dfs(0,i,j, board,word);
+                }
             }
         }
-        return false;
+        return ans;
     }
 };
