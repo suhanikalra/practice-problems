234c28fcf6feb0d8bb42e2f275b293c9d676d6b2
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-02-15 21:11:32 +0530

    Time: 4 ms (65.94%), Space: 20.3 MB (19.94%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>adj(numCourses);
-        
-         int n = adj.size();
-
-        queue<int>q;
-        vector<int>indegree(n,0);int i=0;
-        for(auto a: prerequisites){
-            adj[a[1]].push_back(a[0]);
-            indegree[a[0]]++;
-            } 
-
-        for(int i=0;i<n;i++){
-                if(indegree[i]==0)q.push(i);
-            }
-            vector<int>ans;
-
-        while(!q.empty()){
-            auto ele= q.front();q.pop();
-            ans.push_back(ele);
-            for(auto nbr: adj[ele]){
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+         vector<vector<int>>adjList(n);vector<int>indegree(n,0);
+         for(auto e: prerequisites){
+            auto x=e[0];auto y=e[1];
+            adjList[y].push_back(x);
+            indegree[x]++;
+         }
+         queue<int>q;
+         for(int i=0;i<n;i++){
+            if(indegree[i]==0)q.push(i);
+         }
+vector<int>ans;
+         while(!q.empty()){
+            auto ele= q.front();
+            q.pop(); ans.push_back(ele);
+            for( auto nbr: adjList[ele]){
                 indegree[nbr]--;
-                if(indegree[nbr]==0)q.push(nbr);
+                if(indegree[nbr]==0){q.push(nbr);}
             }
+         }
+    
 
-
-        }
-        return ans.size()==n;
-        
-
-
-
+         return ans.size()==n;
+         
     }
 };
