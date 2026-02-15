4f895d87e2ed039fe5d0b216dccbb2b67ce589f5
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-02-15 20:27:30 +0530

    Time: 1 ms (20.53%), Space: 18.3 MB (44.26%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int originalColor = image[sr][sc];
-        
-        if (originalColor == color) return image;
-        
-        int rows = image.size();
-        int cols = image[0].size();
-        
-        queue<pair<int,int>> q;
+        int m= image.size();int n=image[0].size();int srccolor=image[sr][sc];
+        if( srccolor==color)return image;
+        queue<pair<int,int>>q;
         q.push({sr,sc});
-        image[sr][sc] = color;  
-        
-        int dx[] = {-1, 1, 0, 0};
-        int dy[] = {0, 0, -1, 1};
-        
-        while(!q.empty()) {
-            auto ele = q.front();
+        int dx[]={1,-1,0,0};
+        int dy[]={0,0,1,-1};
+
+        while( !q.empty()){
+            auto ele= q.front();
             q.pop();
-            int x = ele.first;
-            int y = ele.second;
-            
-            for(int i = 0; i < 4; i++) {
-                int newx = x + dx[i];
-                int newy = y + dy[i];
-                
-                if(newx >= 0 && newx < rows && 
-                   newy >= 0 && newy < cols && 
-                   image[newx][newy] == originalColor) {
-                    image[newx][newy] = color;
-                    q.push({newx, newy});
-                }
+            auto x=ele.first;
+            auto y= ele.second;
+            image[x][y]=color;
+            for( int i=0; i<4; i++){
+                int newx=x+dx[i];
+                int newy= y+dy[i];
+                if (newx >= 0 && newx < m &&
+                    newy >= 0 && newy < n &&
+                    image[newx][newy] == srccolor){
+                        
+                        q.push({newx,newy});
+                    } 
             }
         }
-        
         return image;
+        
     }
 };
