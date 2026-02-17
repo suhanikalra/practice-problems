fd52f2abd814c8275ac5cb15da41a30aa198f042
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-02-17 19:16:35 +0530

    Time: 176 ms (5.7%), Space: 171.3 MB (5.14%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,41 +1,29 @@
 class Solution {
 public:
-vector<vector<string>> ans;
- void helper(string &s, vector<string> &temp, int index) {
-  
-        if (index == s.size()) {
-            ans.push_back(temp);
-            return;
-        }
-
-        string pal_string;
-        
-        for (int i = index; i < s.size(); i++) {
-            pal_string = s.substr(index, i - index + 1);  
-            if (isPalindrome(pal_string)) {  
-                temp.push_back(pal_string);  
-                helper(s, temp, i + 1); 
-                temp.pop_back();  
-            }
-        }
-    }
-
-    
-    bool isPalindrome(string &s) {
-        int left = 0, right = s.size() - 1;
-        while (left < right) {
-            if (s[left] != s[right])
-                return false;
-            left++;
-            right--;
+bool isPal(string s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            if (s[l++] != s[r--]) return false;
         }
         return true;
     }
-
-
+    vector<vector<string>>ans;
+    void solve( int i, string s, vector<string>temp){
+        if( i== s.size()){
+            ans.push_back(temp);return;
+        }
+        for( int ind= i;ind<s.size();ind++){
+            string first = s.substr(i, ind - i + 1);
+            if( isPal(first)){
+                temp.push_back(first);
+                solve(ind+1,s,temp);
+                temp.pop_back();
+            }
+        }
+    }
     vector<vector<string>> partition(string s) {
-        vector<string> temp;
-        helper(s,temp,0);
+        vector<string>temp;
+        solve( 0, s, temp);
         return ans;
     }
 };
