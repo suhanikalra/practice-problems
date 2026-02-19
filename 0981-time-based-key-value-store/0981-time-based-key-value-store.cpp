eec396f3736995b5ad5d5f44d02aeedaecfe09c8
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-02-19 09:26:52 +0530

    Time: 60 ms (39.01%), Space: 137.1 MB (46.44%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -1,32 +1,30 @@
 class TimeMap {
 public:
-unordered_map<string,vector<pair<int,string>>>mp;
+unordered_map<string,vector<pair<int, string>>>mp;
     TimeMap() {
         
     }
     
-    void set(string key, string value, int timestamp) {
-       mp[key].push_back({timestamp,value});
+    void set(string key, string value, int time) {
+        mp[key].push_back({time,value});
     }
     
     string get(string key, int timestamp) {
-        
-        if( mp.find( key)== mp.end())return "";
-        auto &nums= mp[key];
-        int low= 0;int high= nums.size()-1;
-        int mid;int ind=-1;
-        while(low<=high){
-            mid= (high-low)/2+low;
-            if(nums[mid].first==timestamp){return nums[mid].second;}
-            else if( nums[mid].first>timestamp){
-                high= mid-1;
+        if( mp.find(key)==mp.end())return "";
+        auto &nums= mp[key];string ans= "";
+        int start= 0;int end= nums.size()-1;
+        int mid= (end-start)/2+start;
 
+        while(start<=end){
+            mid= (end-start)/2+start;
+            if( nums[mid].first==timestamp){ans=nums[mid].second;return ans;}
+            else if(nums[mid].first > timestamp ){
+                end= mid-1;
             }
-            else {ind=mid;low= mid+1;}
-        }
-        if (ind == -1) return "";
-        return nums[ind].second;
+            else{ ans= nums[mid].second; start= mid+1;}
 
+        }
+        return ans;
         
     }
 };
