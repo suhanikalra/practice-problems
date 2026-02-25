4e48d5bd9aa4e0c0a7f0b9531f5662f753630eeb
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-02-25 09:37:57 +0530

    Time: 67 ms (74.35%), Space: 172.9 MB (86.83%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,23 +1,25 @@
-class Node {
-public:
-    int key;
-    int value;
-    Node* prev;
+class Node{
+    public:
+
     Node* next;
+    Node* prev;
+    int key,val;
 
-    Node(int key, int value) {
-        this->key = key;
-        this->value = value;
-        prev = NULL;
+    Node(int k,int v){
+        this->key=k;
+        this->val= v;
         next = NULL;
+        prev = NULL;
     }
+    
 };
-
 class LRUCache {
 public:
-Node* head;
-Node* tail;
-    void deleteNode(Node* node) {
+int cap=0;
+unordered_map<int,Node*>cacheMap;
+Node* head;Node* tail;
+
+void deleteNode(Node* node) {
         node->prev->next = node->next;
         node->next->prev = node->prev;
     }
@@ -28,43 +30,41 @@ Node* tail;
         node->prev = head;
         head->next = node;
     }
-
-    int cap;
-    unordered_map<int, Node*> mp;
-    LRUCache(int capacity) { 
-        cap = capacity; 
-        head = new Node(-1, -1);
-        tail = new Node(-1, -1);
+    LRUCache(int capacity) {
+        head= new Node(-1,-1);
+        tail= new Node(-1,-1);
+        this->cap= capacity;
         head->next= tail;
-        tail->prev= head;}
-
-    int get(int key) {
-        if (mp.find(key) != mp.end()) {
-            auto node = mp[key];
-            deleteNode(node);
-            addNode(node);
-            return node->value;
-        }
-        return -1;
+        tail->prev= head;
     }
-
+    
+    int get(int key) {
+        if( cacheMap.find(key)==cacheMap.end())return -1;
+        auto node= cacheMap[key];
+        auto ans= node->val;
+        deleteNode(node);
+        addNode(node);
+        return ans;
+    }
+    
     void put(int key, int value) {
-        if (mp.find(key) != mp.end()) {
-            auto node = mp[key];
-            node->value = value;
+        if (cacheMap.find(key) != cacheMap.end()) {
+            auto node = cacheMap[key];
+            node->val = value;
             deleteNode(node);
             addNode(node);
 
         } else {
-            if (mp.size() == cap) {
+            if (cacheMap.size() == cap) {
                 auto lastnode = tail->prev;
-                mp.erase(lastnode->key);
+                cacheMap.erase(lastnode->key);
                 deleteNode(lastnode);
             }
 
-            mp[key] = new Node(key,value);
-            addNode(mp[key]);
-        }
+            cacheMap[key] = new Node(key,value);
+            addNode(cacheMap[key]);
+        
+        }    
     }
 };
 
