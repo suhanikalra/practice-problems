943583c4f2d2b7d219b1ba56be1cc8e342a4cd1b
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-02-28 14:15:56 +0530

    Time: 23 ms (67.7%), Space: 56.4 MB (61.24%) - LeetHub

--- 2488-count-subarrays-with-median-k/2488-count-subarrays-with-median-k.cpp
+++ 2488-count-subarrays-with-median-k/2488-count-subarrays-with-median-k.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int countSubarrays(vector<int>& nums, int k) {
-         unordered_map<int,int> mp;bool found=false;
+        unordered_map<int,int>mp;int i=0;
         mp[0] = 1; int sum=0;int ans=0;
-        for( auto num: nums){
-            if( num>k){
-                sum+=1;
-            }
-            else if( num<k){sum-=1;}
-            else if(num==k)found=true;
-            if( found){
-                ans=ans+ mp[sum]+mp[sum-1];
-            }
-            else {
-                mp[sum]++;
-            }
+        for(;i<nums.size();i++){
+            auto num= nums[i];
+            if(num>k)sum++;
+            if(num<k)sum--;
+            if(num==k)break;
+            mp[sum]++;
+        }
+        for(;i<nums.size();i++){
+            auto num= nums[i];
+            if(num>k)sum++;
+            if(num<k)sum--;
+            ans=ans+ mp[sum]+mp[sum-1];
 
         }
         return ans;
+
     }
 };
