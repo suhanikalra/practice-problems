50da273b93ebf0dfbb2d6bdb3f653faec99be52e
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-03-02 13:43:44 +0530

    Time: 1519 ms (5.16%), Space: 600.5 MB (5.16%) - LeetHub

--- 2296-design-a-text-editor/2296-design-a-text-editor.cpp
+++ 2296-design-a-text-editor/2296-design-a-text-editor.cpp
@@ -1,72 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 class TextEditor {
+private:
+    string left;   // text to the left of cursor
+    string right;  // text to the right of cursor
+
+    string getLast10() {
+        int start = max(0, (int)left.size() - 10);
+        return left.substr(start);
+    }
+
 public:
-    stack<char> left, right;
     TextEditor() {}
 
     void addText(string text) {
-        for (auto c : text) {
-            left.push(c);
-        }
+        left += text; 
     }
 
     int deleteText(int k) {
-        int count = 0;
-        while (!left.empty() and count < k) {
-            left.pop();
-            count++;
-        }
-        return count;
+        int deleted = min(k, (int)left.size());
+        left.erase(left.size() - deleted, deleted);
+        return deleted;
     }
-    string getLast10() {
-    string ans = "";
-    vector<char> buffer;
-
-    int count = 0;
-    while (!left.empty() && count < 10) {
-        buffer.push_back(left.top());
-        left.pop();
-        count++;
-    }
-
-    // rebuild string in correct order
-    for (int i = buffer.size() - 1; i >= 0; i--) {
-        ans += buffer[i];
-        left.push(buffer[i]);  // restore stack
-    }
-
-    return ans;
-}
 
     string cursorLeft(int k) {
-        string ans = "";
-        int count = 0;
-        while (!left.empty() and count < k) {
-            right.push(left.top());
-            auto a = left.top();
-            left.pop();
-            count++;
-        }
+        int move = min(k, (int)left.size());
+
+        right = left.substr(left.size() - move) + right;
+        left.erase(left.size() - move, move);
+
         return getLast10();
     }
 
     string cursorRight(int k) {
-        string ans = "";
-        int count = 0;
-        while (!right.empty() and count < k) {
-            left.push(right.top());
-            auto a = right.top();
-            right.pop();
-            count++;
-        }
+        int move = min(k, (int)right.size());
+
+        left += right.substr(0, move);
+        right.erase(0, move);
+
         return getLast10();
     }
 };
-
-/**
- * Your TextEditor object will be instantiated and called as such:
- * TextEditor* obj = new TextEditor();
- * obj->addText(text);
- * int param_2 = obj->deleteText(k);
- * string param_3 = obj->cursorLeft(k);
- * string param_4 = obj->cursorRight(k);
- */
