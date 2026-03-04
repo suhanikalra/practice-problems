bc32f388556b31a98fe3a25a02d61ac8ed463a69
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-03-04 22:07:44 +0530

    Time: 123 ms (13.39%), Space: 55.1 MB (5.88%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+bool solve( int i,int j,string &s, string &p, vector<vector<int>>&dp ){
+    if (j == p.size()) return i == s.size();
+    if (i == s.size()) {
+            while (j < p.size() && p[j] == '*') j++;
+            return j == p.size();
+        }  
+    if(dp[i][j]!=-1)return dp[i][j]; 
+
+    if(p[j]=='?'){return dp[i][j]= solve(i+1,j+1,s,p,dp);}
+    else if(p[j]=='*'){return dp[i][j]= solve(i,j+1,s,p,dp) or solve(i+1,j,s,p,dp);}
+    else if(s[i]==p[j]){return dp[i][j]= solve(i+1,j+1,s,p,dp);}
+    return false;
+}
+    bool isMatch(string s, string p) {
+        vector<vector<int>>dp(s.size()+1,vector<int>(p.size()+1,-1));
+       return  solve(0,0,s,p,dp);
+    }
+};
