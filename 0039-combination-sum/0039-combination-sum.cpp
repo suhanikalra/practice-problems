dfd90f81c7d1394957e1b426401968a2d9a07a62
suhanikalra <95181779+suhanikalra@users.noreply.github.com>
2026-03-05 12:09:39 +0530

    Time: 55 ms (10.58%), Space: 57.2 MB (8.85%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-    void solve(int i, vector<int>& candidates, int target, vector<int>& ans) {
-        if (target == 0) {
-            res.push_back(ans);
-            return;
-        }
-        if (target < 0 or i == candidates.size())
-            return;
-        if (candidates[i] <= target) {
-            ans.push_back(candidates[i]);
-            solve(i, candidates, target - candidates[i], ans);
-            ans.pop_back();
-        } 
-            solve(i + 1, candidates, target , ans);
+vector<vector<int>> res;
+
+void solve(int i, vector<int>& candidates, int target,vector<int>ans){
+    
+    if(i==candidates.size() and target==0){res.push_back(ans);return;}
+    if( i== candidates.size())return;
+
+    if(candidates[i]<=target){
+        ans.push_back(candidates[i]);
+        solve(i,candidates,target-candidates[i],ans);
+        ans.pop_back();
+        solve(i+1,candidates,target,ans);
+
     }
+    else solve(i+1,candidates,target,ans);
+
+}
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> ans;
-        solve(0, candidates, target, ans);
+        solve(0,candidates, target,{});
         return res;
     }
 };
